f7961a2289836b18872e70ed9a4195842e50efe1
Blagovest Georgiev <47055593+bgeorgieff@users.noreply.github.com>
2022-07-16 14:47:12 +0300

    added login and register logics (#9)
    - additionally refactored enums and interfaces, added missing params

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotAcceptableException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IUser } from 'src/interfaces/IUser.interface';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Messages } from 'src/enums/messages.enum';
+import { IBearer } from 'src/interfaces/IBearer.interface';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<IUser>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async create(
+    createUserDto: CreateUserDto,
+  ): Promise<{ message: string } | undefined> {
+    try {
+      const { password } = createUserDto;
+      const saltRounds = 10;
+      const hashedPass = await bcrypt.hash(password, saltRounds);
+      createUserDto.password = hashedPass;
+      await new this.userModel(createUserDto).save();
+
+      return { message: Messages.userRegistered };
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async login(userEmail: Partial<CreateUserDto>): Promise<IBearer | undefined> {
+    try {
+      const { email, isAdmin, _id } = await this.userModel.findOne({
+        email: userEmail,
+      });
+      const payload = { email, isAdmin, _id };
+
+      return {
+        token: this.jwtService.sign(payload),
+      };
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userModel.findOne({ email });
+    if (!user) {
+      throw new NotAcceptableException(Messages.couldntFindUser);
+    }
+    const passwordValid = await bcrypt.compare(password, user.password);
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    if (user && passwordValid) {
+      const { ...result } = user;
+      return result;
+    } else {
+      throw new UnauthorizedException(Messages.wrongPassword);
+    }
   }
 }
